af6e5d44d3d5003de4fd7ad57464ee07c6e37b92
Farhana Shahid <farhana.shahidcse@gmail.com>
2026-04-30 14:34:51 -0400

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-0c1198d60d910f3b2b77.js"
+    "url": "webpack-runtime-df2ef03f9745625309d1.js"
   },
   {
     "url": "framework-69f9573f685b242367c3.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "22089ae3f940261c8879fa3e5b3f399d"
+    "revision": "8d635ffba55c05742e86ba15297b5be6"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-61121c60ba413d2aba44.js"
